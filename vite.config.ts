e846c5bf61bc8562a5ffa8f40d2ba389caa65a17
Mariana M <github_618a@imariana.com>
2025-06-22 11:43:56 +0100

    feat: display git version in footer

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { execSync } from "child_process";
+
+const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
+const commitDate = execSync(
+  "git show -s --date=format:%y%m%d --format=%cd"
+).toString().trim();
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -18,5 +24,9 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  define: {
+    "import.meta.env.VITE_COMMIT_HASH": JSON.stringify(commitHash),
+    "import.meta.env.VITE_COMMIT_DATE": JSON.stringify(commitDate),
   },
 }));
