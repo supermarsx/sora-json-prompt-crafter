4150788fee418132438eea62cdc0704d07fa4c36
Mariana M <github_618a@imariana.com>
2025-07-09 22:39:29 +0100

    fix: suppress React Router future warning

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ const App = () => {
           <TooltipProvider>
             <Toaster />
             <Sonner />
-            <BrowserRouter>
+            <BrowserRouter future={{ v7_startTransition: true }}>
               <Suspense fallback={null}>
                 <Routes>
                   <Route path="/" element={<Index />} />
