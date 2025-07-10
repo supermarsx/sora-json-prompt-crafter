e0f6b3abeef9176a5a785a8182a6a155dfdbd7e1
Mariana M <github_618a@imariana.com>
2025-07-10 01:00:48 +0100

    test: validate option flag map

--- src/lib/__tests__/optionFlagMap.test.ts
+++ src/lib/__tests__/optionFlagMap.test.ts
@@ -1,6 +1,13 @@
 import { OPTION_FLAG_MAP } from '../optionFlagMap';
+import { DEFAULT_OPTIONS } from '../defaultOptions';
 
 describe('OPTION_FLAG_MAP', () => {
+  test('every flag exists on DEFAULT_OPTIONS', () => {
+    for (const flag of Object.values(OPTION_FLAG_MAP)) {
+      expect(flag in DEFAULT_OPTIONS).toBe(true);
+    }
+  });
+
   test('returns correct flag for lighting', () => {
     expect(OPTION_FLAG_MAP.lighting).toBe('use_lighting');
   });
@@ -9,8 +16,8 @@ describe('OPTION_FLAG_MAP', () => {
     expect(OPTION_FLAG_MAP.secondary_material).toBe('use_secondary_material');
   });
 
-  test('returns correct flag for camera_angle', () => {
-    expect(OPTION_FLAG_MAP.camera_angle).toBe('use_camera_composition');
+  test('returns correct flag for dnd_character_race', () => {
+    expect(OPTION_FLAG_MAP.dnd_character_race).toBe('use_dnd_character_race');
   });
 
   test('returns undefined for nonexistent key', () => {
