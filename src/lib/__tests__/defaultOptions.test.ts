a2f9cdf4addd3facc80adbf1899bcc695470a9ac
Mariana M <github_618a@imariana.com>
2025-07-13 22:09:14 +0100

    test: validate default options

--- src/lib/__tests__/defaultOptions.test.ts
+++ src/lib/__tests__/defaultOptions.test.ts
@@ -1,5 +1,5 @@
-import { DEFAULT_OPTIONS } from '../defaultOptions';
-import { isValidOptions } from '../validateOptions';
+import { DEFAULT_OPTIONS } from '@/lib/defaultOptions';
+import { isValidOptions } from '@/lib/validateOptions';
 
 describe('DEFAULT_OPTIONS', () => {
   test('validate as acceptable options', () => {
