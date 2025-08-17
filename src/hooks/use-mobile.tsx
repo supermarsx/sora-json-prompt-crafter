e54415af0ffc77721058f07be1f01bd813623c68
Mariana <github_618a@imariana.com>
2025-08-17 23:33:43 +0100

    docs: document responsive hooks

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react';
 import { MOBILE_BREAKPOINT } from '@/lib/breakpoints';
 
+/**
+ * Determines if the viewport should be treated as mobile.
+ *
+ * A mobile viewport is defined as one with a width below
+ * `MOBILE_BREAKPOINT` (768px).
+ *
+ * @returns {boolean} `true` if the viewport width is less than
+ * `MOBILE_BREAKPOINT`, otherwise `false`.
+ */
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState(false);
 
