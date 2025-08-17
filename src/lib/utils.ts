9cb3a3604d4765447a3203e0ac40cad18e31a46e
Mariana <github_618a@imariana.com>
2025-08-17 22:57:20 +0100

    docs: add JSDoc for cn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+/**
+ * Combines Tailwind CSS class names, resolving conflicts.
+ *
+ * @param inputs - Class values to merge.
+ * @returns The merged class string.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
